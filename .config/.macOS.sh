c3aa10b0bf2b7eccb36dad4edb7443f5b267afca
Austin Paxton <apaxton@teladoc.com>
2016-10-20 12:05:39 -0500

    add inital macOS defaults file

---
+++ .config/.macOS.sh
@@ -0,0 +1,6 @@
+# Disable menu bar transparency
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+
+# Show remaining battery time; hide percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+defaults write com.apple.menuextra.battery ShowTime -string "YES"
